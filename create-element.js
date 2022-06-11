ab928a8ac16e9e4b3bad1c1966e775a54c3b8b57
riwall <andrey.riwall@gmail.com>
2022-06-12 01:42:33 +0300

    update

---
+++ create-element.js
@@ -0,0 +1,19 @@
+function createEl (userConfig = new Object()) {
+    let defaulConfig = {
+        tag: 'div',
+        parent: document.body,
+        class: null,
+        id: null,
+        text: null,
+    }
+    let newConfig = { ...defaulConfig, ...userConfig }
+    
+    let item = document.createElement(newConfig.tag);
+    if (newConfig.class != null) item.setAttribute('class', newConfig.class);
+    if (newConfig.id != null) item.id = newConfig.id;
+    if (newConfig.text != null) item.textContent = newConfig.text;
+    newConfig.parent = newConfig.parent === null ? document.body : newConfig.parent;
+    (newConfig.parent).append(item);
+
+    return item;
+}
